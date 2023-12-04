ff8d0efa0e45c501c3fda2fc37e2b8bf30a9e481
Stray8 <2086312274@qq.com>
2023-12-04 18:08:01 +0800

    fix chinese code

--- MOTION_CONTROL/DragTeach.c
+++ MOTION_CONTROL/DragTeach.c
@@ -1,34 +1,36 @@
 #include "DragTeach.h"
 
-double K[2] = {0.532,0.232};  //Ħ����б��
-double B[2] = {5,5};	//Ħ�����ؾ�
+double K[2] = {0.532,0.232};  //摩擦力斜率
+double B[2] = {5,5};	//摩擦力截距
 
 s32 Vel = 0;
 
-// void TorqueProcess()
-// {
-// 	int i = 0;
-// 	s16 tempTorque = 0;
-// 	s32 tempVel = 0;
+void TorqueProcess()
+{
+	int i = 0;
+	s16 tempTorque = 0;
+	s32 tempVel = 0;
 	
 	
 	
-// 	for(i=1; i<TEACH_AXIS_NUM; i++)
+	for(i=1; i<TEACH_AXIS_NUM; i++)
+	{
 		
-// 		tempVel = ServoMoveCurSpeed(i);
-// 		if (tempVel != ServoMoveCurSpeed(i) )
-// 		{
-// 			if(tempVel > 0)
-// 				tempTorque = ( K[i]*tempVel + B[i] ) * 10; //��λΪ0.1%
-// 			else
-// 				tempTorque = ( K[i]*tempVel - B[i] ) * 10;
+		tempVel = ServoMoveCurSpeed(i);
+		if (tempVel != ServoMoveCurSpeed(i) )
+		{
+			if(tempVel > 0)
+				tempTorque = ( K[i]*tempVel + B[i] ) * 10; //单位为0.1%
+			else
+				tempTorque = ( K[i]*tempVel - B[i] ) * 10;
 			
-// 			ServoCST_PDO(i,tempTorque);
-// 		}
+			ServoCST_PDO(i,tempTorque);
+		}
 		
-// 	}
+	}
+	
 
-
+}
 
 void TorqueProcess_T()
 {
@@ -37,30 +39,51 @@ void TorqueProcess_T()
 	s32 tempVel = 0;
 	
 	
-	for(i = 1; i < TEACH_AXIS_NUM+1; i++)
-	{
+	
+	for(i = 1; i<= TEACH_AXIS_NUM; i++){	
 		tempVel = ServoMoveCurSpeed(i);
-		if (1)//(tempVel != ServoMoveCurSpeed(i) )
-		{
-			if(tempVel > ZERO_ERR)
-			{
-				tempTorque = AXIS_01_TORQUE_START; //��λΪ0.1%
+		
+		if (1){			//(tempVel != ServoMoveCurSpeed(i) )
+			if(tempVel > ZERO_ERR){
+				tempTorque = AXIS_01_TORQUE_START; //单位为0.1%
 			}
-			else if(tempVel <= ZERO_ERR && tempVel >= (-1*ZERO_ERR))
-			{
-				if(Torque_T_count%(2*ZERO_CHANGE_CYCLE) <= ZERO_CHANGE_CYCLE)
-					tempTorque = AXIS_01_TORQUE_START*ZERO_CHANGE_COE;
-				else
-					tempTorque = AXIS_01_TORQUE_START*(-1)*ZERO_CHANGE_COE;
+			else if(tempVel <= ZERO_ERR && tempVel >= 0){
+				tempTorque = AXIS_01_TORQUE_START * tempVel * 0.5; //单位为0.1%
+			}
+			else if (tempVel <= 0 && tempVel >= (-1) * ZERO_ERR){
+				tempTorque = (-1) * AXIS_01_TORQUE_START * tempVel * 0.5; //单位为0.1%
 			}
 			else
 				tempTorque = AXIS_01_TORQUE_START*(-1);
 			
 			ServoCST_PDO(i,tempTorque);
 		}
-		
 	}
-
-
 }
 
+// void TorqueProcess_T()
+// {
+// 	int i = 0;
+// 	s16 tempTorque = 0;
+// 	s32 tempVel = 0;
+	
+// 	for(i = 1; i<= TEACH_AXIS_NUM; i++){	
+// 		tempVel = ServoMoveCurSpeed(i);
+
+// 		if (1){			//(tempVel != ServoMoveCurSpeed(i) )
+// 			if(tempVel > ZERO_ERR){
+// 				tempTorque = AXIS_01_TORQUE_START; //单位为0.1%
+// 			}
+// 			else if(tempVel <= ZERO_ERR && tempVel >= (-1*ZERO_ERR)){
+// 				if(Torque_T_count%(2*ZERO_CHANGE_CYCLE) <= ZERO_CHANGE_CYCLE)
+// 					tempTorque = AXIS_01_TORQUE_START*ZERO_CHANGE_COE;
+// 				else
+// 					tempTorque = AXIS_01_TORQUE_START*(-1)*ZERO_CHANGE_COE;
+// 			}
+// 			else
+// 				tempTorque = AXIS_01_TORQUE_START*(-1);
+			
+// 			ServoCST_PDO(i,tempTorque);
+// 		}
+// 	}
+// }
--- MOTION_CONTROL/DragTeach.h
+++ MOTION_CONTROL/DragTeach.h
@@ -8,9 +8,9 @@
 
 #define TEACH_AXIS_NUM  1
 #define ZERO_ERR  1
-#define ZERO_CHANGE_CYCLE  2  //�ٶ�Ϊ0ʱ�����л����� ��λΪ10ms
-#define ZERO_CHANGE_COE  0.1  //�ٶ�Ϊ0ʱ�����л����� ��λΪ10ms
-#define AXIS_01_TORQUE_START  55   //%0.1
+#define ZERO_CHANGE_CYCLE  2  //速度为0时力矩切换周期 单位为10ms
+#define ZERO_CHANGE_COE  0.1  //速度为0时力矩切换周期 单位为10ms
+#define AXIS_01_TORQUE_START  60    //55   //%0.1
 #define AXIS_02_TORQUE_START  30
 
 
@@ -21,9 +21,6 @@
 
 void TorqueProcess();
 void TorqueProcess_T();
-
-
-
 
 
 
